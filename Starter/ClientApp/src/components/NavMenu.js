11ac4544f8e89dd86f835327fdc84eeb0f93c04d
Muhammad Owais <muhammad.owais9989@gmail.com>
2023-11-27 16:54:44 +0000

    fix layout,nav and routing

--- Starter/ClientApp/src/components/NavMenu.js
+++ Starter/ClientApp/src/components/NavMenu.js
@@ -5,49 +5,53 @@ import LoginMenu from './api-authorization/LoginMenu';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+    static displayName = NavMenu.name;
 
-  constructor (props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true
+        };
+    }
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
+    toggleNavbar() {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
 
-  render() {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">Starter</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-            <ul className="navbar-nav flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/dashboard">Dashboard</NavLink>
-              </NavItem>
-              <LoginMenu>
-              </LoginMenu>
-            </ul>
-          </Collapse>
-        </Navbar>
-      </header>
-    );
-  }
+    render() {
+        const pathName = window.location.pathname;
+        return (
+            <div>{!(pathName.includes("/dashboard")) &&
+                <header>
+                    <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+                        <NavbarBrand tag={Link} to="/">Starter</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                        <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+                            <ul className="navbar-nav flex-grow">
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark" to="/dashboard">Dashboard</NavLink>
+                                </NavItem>
+                                <LoginMenu>
+                                </LoginMenu>
+                            </ul>
+                        </Collapse>
+                    </Navbar>
+                </header>
+
+            }</div>
+        );
+    }
 }
--- Starter/ClientApp/src/routes.js
+++ Starter/ClientApp/src/routes.js
@@ -3,26 +3,26 @@ import React from "react";
 const Dashboard = React.lazy(() => import("./views/dashboard/Dashboard"));
 const Colors = React.lazy(() => import("./views/theme/colors/Colors"));
 const Typography = React.lazy(() =>
-  import("./views/theme/typography/Typography")
+    import("./views/theme/typography/Typography")
 );
 
 // Base
 const Accordion = React.lazy(() => import("./views/base/accordion/Accordion"));
 const Breadcrumbs = React.lazy(() =>
-  import("./views/base/breadcrumbs/Breadcrumbs")
+    import("./views/base/breadcrumbs/Breadcrumbs")
 );
 const Cards = React.lazy(() => import("./views/base/cards/Cards"));
 const Carousels = React.lazy(() => import("./views/base/carousels/Carousels"));
 const Collapses = React.lazy(() => import("./views/base/collapses/Collapses"));
 const ListGroups = React.lazy(() =>
-  import("./views/base/list-groups/ListGroups")
+    import("./views/base/list-groups/ListGroups")
 );
 const Navs = React.lazy(() => import("./views/base/navs/Navs"));
 const Paginations = React.lazy(() =>
-  import("./views/base/paginations/Paginations")
+    import("./views/base/paginations/Paginations")
 );
 const Placeholders = React.lazy(() =>
-  import("./views/base/placeholders/Placeholders")
+    import("./views/base/placeholders/Placeholders")
 );
 const Popovers = React.lazy(() => import("./views/base/popovers/Popovers"));
 const Progress = React.lazy(() => import("./views/base/progress/Progress"));
@@ -33,37 +33,37 @@ const Tooltips = React.lazy(() => import("./views/base/tooltips/Tooltips"));
 // Buttons
 const Buttons = React.lazy(() => import("./views/buttons/buttons/Buttons"));
 const ButtonGroups = React.lazy(() =>
-  import("./views/buttons/button-groups/ButtonGroups")
+    import("./views/buttons/button-groups/ButtonGroups")
 );
 const Dropdowns = React.lazy(() =>
-  import("./views/buttons/dropdowns/Dropdowns")
+    import("./views/buttons/dropdowns/Dropdowns")
 );
 
 //Forms
 const ChecksRadios = React.lazy(() =>
-  import("./views/forms/checks-radios/ChecksRadios")
+    import("./views/forms/checks-radios/ChecksRadios")
 );
 const FloatingLabels = React.lazy(() =>
-  import("./views/forms/floating-labels/FloatingLabels")
+    import("./views/forms/floating-labels/FloatingLabels")
 );
 const FormControl = React.lazy(() =>
-  import("./views/forms/form-control/FormControl")
+    import("./views/forms/form-control/FormControl")
 );
 const InputGroup = React.lazy(() =>
-  import("./views/forms/input-group/InputGroup")
+    import("./views/forms/input-group/InputGroup")
 );
 const Layout = React.lazy(() => import("./views/forms/layout/Layout"));
 const Range = React.lazy(() => import("./views/forms/range/Range"));
 const Select = React.lazy(() => import("./views/forms/select/Select"));
 const Validation = React.lazy(() =>
-  import("./views/forms/validation/Validation")
+    import("./views/forms/validation/Validation")
 );
 
 const Charts = React.lazy(() => import("./views/charts/Charts"));
 
 // Icons
 const CoreUIIcons = React.lazy(() =>
-  import("./views/icons/coreui-icons/CoreUIIcons")
+    import("./views/icons/coreui-icons/CoreUIIcons")
 );
 const Flags = React.lazy(() => import("./views/icons/flags/Flags"));
 const Brands = React.lazy(() => import("./views/icons/brands/Brands"));
@@ -77,70 +77,70 @@ const Toasts = React.lazy(() => import("./views/notifications/toasts/Toasts"));
 const Widgets = React.lazy(() => import("./views/widgets/Widgets"));
 
 const routes = [
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    element: Dashboard,
-  },
-  { path: "/theme", name: "Theme", element: Colors, exact: true },
-  { path: "/theme/colors", name: "Colors", element: Colors },
-  { path: "/theme/typography", name: "Typography", element: Typography },
-  { path: "/base", name: "Base", element: Cards, exact: true },
-  { path: "/base/accordion", name: "Accordion", element: Accordion },
-  { path: "/base/breadcrumbs", name: "Breadcrumbs", element: Breadcrumbs },
-  { path: "/base/cards", name: "Cards", element: Cards },
-  { path: "/base/carousels", name: "Carousel", element: Carousels },
-  { path: "/base/collapses", name: "Collapse", element: Collapses },
-  { path: "/base/list-groups", name: "List Groups", element: ListGroups },
-  { path: "/base/navs", name: "Navs", element: Navs },
-  { path: "/base/paginations", name: "Paginations", element: Paginations },
-  { path: "/base/placeholders", name: "Placeholders", element: Placeholders },
-  { path: "/base/popovers", name: "Popovers", element: Popovers },
-  { path: "/base/progress", name: "Progress", element: Progress },
-  { path: "/base/spinners", name: "Spinners", element: Spinners },
-  { path: "/base/tables", name: "Tables", element: Tables },
-  { path: "/base/tooltips", name: "Tooltips", element: Tooltips },
-  { path: "/buttons", name: "Buttons", element: Buttons, exact: true },
-  { path: "/buttons/buttons", name: "Buttons", element: Buttons },
-  { path: "/buttons/dropdowns", name: "Dropdowns", element: Dropdowns },
-  {
-    path: "/buttons/button-groups",
-    name: "Button Groups",
-    element: ButtonGroups,
-  },
-  { path: "/charts", name: "Charts", element: Charts },
-  { path: "/forms", name: "Forms", element: FormControl, exact: true },
-  { path: "/forms/form-control", name: "Form Control", element: FormControl },
-  { path: "/forms/select", name: "Select", element: Select },
-  {
-    path: "/forms/checks-radios",
-    name: "Checks & Radios",
-    element: ChecksRadios,
-  },
-  { path: "/forms/range", name: "Range", element: Range },
-  { path: "/forms/input-group", name: "Input Group", element: InputGroup },
-  {
-    path: "/forms/floating-labels",
-    name: "Floating Labels",
-    element: FloatingLabels,
-  },
-  { path: "/forms/layout", name: "Layout", element: Layout },
-  { path: "/forms/validation", name: "Validation", element: Validation },
-  { path: "/icons", exact: true, name: "Icons", element: CoreUIIcons },
-  { path: "/icons/coreui-icons", name: "CoreUI Icons", element: CoreUIIcons },
-  { path: "/icons/flags", name: "Flags", element: Flags },
-  { path: "/icons/brands", name: "Brands", element: Brands },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    element: Alerts,
-    exact: true,
-  },
-  { path: "/notifications/alerts", name: "Alerts", element: Alerts },
-  { path: "/notifications/badges", name: "Badges", element: Badges },
-  { path: "/notifications/modals", name: "Modals", element: Modals },
-  { path: "/notifications/toasts", name: "Toasts", element: Toasts },
-  { path: "/widgets", name: "Widgets", element: Widgets },
+    {
+        path: "/dashboard",
+        name: "Dashboard",
+        element: Dashboard,
+    },
+    { path: "/dashboard/theme", name: "Theme", element: Colors, exact: true },
+    { path: "/dashboard/theme/colors", name: "Colors", element: Colors },
+    { path: "/dashboard/theme/typography", name: "Typography", element: Typography },
+    { path: "/dashboard/base", name: "Base", element: Cards, exact: true },
+    { path: "/dashboard/base/accordion", name: "Accordion", element: Accordion },
+    { path: "/dashboard/base/breadcrumbs", name: "Breadcrumbs", element: Breadcrumbs },
+    { path: "/dashboard/base/cards", name: "Cards", element: Cards },
+    { path: "/dashboard/base/carousels", name: "Carousel", element: Carousels },
+    { path: "/dashboard/base/collapses", name: "Collapse", element: Collapses },
+    { path: "/dashboard/base/list-groups", name: "List Groups", element: ListGroups },
+    { path: "/dashboard/base/navs", name: "Navs", element: Navs },
+    { path: "/dashboard/base/paginations", name: "Paginations", element: Paginations },
+    { path: "/dashboard/base/placeholders", name: "Placeholders", element: Placeholders },
+    { path: "/dashboard/base/popovers", name: "Popovers", element: Popovers },
+    { path: "/dashboard/base/progress", name: "Progress", element: Progress },
+    { path: "/dashboard/base/spinners", name: "Spinners", element: Spinners },
+    { path: "/dashboard/base/tables", name: "Tables", element: Tables },
+    { path: "/dashboard/base/tooltips", name: "Tooltips", element: Tooltips },
+    { path: "/dashboard/buttons", name: "Buttons", element: Buttons, exact: true },
+    { path: "/dashboard/buttons/buttons", name: "Buttons", element: Buttons },
+    { path: "/dashboard/buttons/dropdowns", name: "Dropdowns", element: Dropdowns },
+    {
+        path: "/dashboard/buttons/button-groups",
+        name: "Button Groups",
+        element: ButtonGroups,
+    },
+    { path: "/dashboard/charts", name: "Charts", element: Charts },
+    { path: "/dashboard/forms", name: "Forms", element: FormControl, exact: true },
+    { path: "/dashboard/forms/form-control", name: "Form Control", element: FormControl },
+    { path: "/dashboard/forms/select", name: "Select", element: Select },
+    {
+        path: "/dashboard/forms/checks-radios",
+        name: "Checks & Radios",
+        element: ChecksRadios,
+    },
+    { path: "/dashboard/forms/range", name: "Range", element: Range },
+    { path: "/dashboard/forms/input-group", name: "Input Group", element: InputGroup },
+    {
+        path: "/dashboard/forms/floating-labels",
+        name: "Floating Labels",
+        element: FloatingLabels,
+    },
+    { path: "/dashboard/forms/layout", name: "Layout", element: Layout },
+    { path: "/dashboard/forms/validation", name: "Validation", element: Validation },
+    { path: "/dashboard/icons", exact: true, name: "Icons", element: CoreUIIcons },
+    { path: "/dashboard/icons/coreui-icons", name: "CoreUI Icons", element: CoreUIIcons },
+    { path: "/dashboard/icons/flags", name: "Flags", element: Flags },
+    { path: "/dashboard/icons/brands", name: "Brands", element: Brands },
+    {
+        path: "/dashboard/notifications",
+        name: "Notifications",
+        element: Alerts,
+        exact: true,
+    },
+    { path: "/dashboard/notifications/alerts", name: "Alerts", element: Alerts },
+    { path: "/dashboard/notifications/badges", name: "Badges", element: Badges },
+    { path: "/dashboard/notifications/modals", name: "Modals", element: Modals },
+    { path: "/dashboard/notifications/toasts", name: "Toasts", element: Toasts },
+    { path: "/dashboard/widgets", name: "Widgets", element: Widgets },
 ];
 
 export default routes;
